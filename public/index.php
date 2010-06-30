cdd15aa0ce430702d61461aac3f8316d2712ae00
thrashr888 <thrashr888@gmail.com>
2010-06-29 22:00:26 -0700

    docs, remove mongo admin

--- public/index.php
+++ public/index.php
@@ -7,9 +7,10 @@ require '../vendor/Bogart/lib/Bogart/App.php';
 try
 {
   $app = new App('index', 'dev', true);
+  //$app = new App('../index.php', 'dev', true); # allow this too. above is default.
   $app->run();
 }
 catch(\Exception $e)
 {
-  print_r($e);
+  //print_r($e);
 }
